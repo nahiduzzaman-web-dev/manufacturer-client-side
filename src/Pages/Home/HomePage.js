648aed1b904508ea625930b6227ac43e2fc0e58d
nahiduzzaman2021 <akash.nahid.27@gmail.com>
2022-05-22 04:34:11 +0600

    footer & 1 extra section

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Footer from '../Shared/Footer';
 import Banner from './Banner';
+import Offer from './Offer';
 
 const HomePage = () => {
     return (
         <div className=''>
             <Banner></Banner>
+            <Offer></Offer>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/Pages/Home/Offer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import offerImg from '../../images/offer.jpg';
+import offerBgImg from '../../images/offerBG.jpg';
+
+const Offer = () => {
+    return (
+        <div
+            style={{
+                background: `url(${offerBgImg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat',
+            }}
+        >
+            <div class="hero ">
+                <div class="hero-content flex-col lg:flex-row-reverse">
+                    <div class="text-center lg:text-left">
+                        <h1 class="text-5xl font-bold">SubsCribe now!</h1>
+                        <p class="py-6">You might also choose to use promotional text messaging for particular sales or discounts on offer. Research shows that customers are more likely to make a purchase when offered a deal. In fact, 67 percent of consumers admitted to making a purchase they hadn’t been planning to make because they got a deal or discount. Here are some examples of promotional text messages you might use for your business.</p>
+                        <button class="btn btn-accent">Subscribe Here</button>
+                    </div>
+                    <div class="card flex-shrink-0 w-full max-w-md shadow-2xl">
+                        <div class="">
+                            <img src={offerImg} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Offer;
